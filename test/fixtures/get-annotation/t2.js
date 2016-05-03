246b3e4990d7db4654a5aa6b8c138767d0b4b863
bencode <bencode@163.com>
2016-05-03 23:27:07 +0800

    feat: first workable version

---
+++ test/fixtures/get-annotation/t2.js
@@ -0,0 +1,21 @@
+/**
+ * 下面是一个无注释
+ *
+ * @entry
+ *
+ * @moduleId: 'test/my-module'
+ *
+ * @lessbuild: false
+ *
+ * @path: "/a/b-c/hello"
+ */
+
+/**
+ * hello
+ *
+ * @preventBuild
+ */
+
+const a = 123;
+a = a;
+
